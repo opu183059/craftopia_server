fdef59df64c90b687612a56a2037a0b9e4bc0909
opu183059 <opuctg440@gmail.com>
2023-06-07 22:57:03 +0600

    Class delete function added for instructor

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -92,6 +92,14 @@ async function run() {
       })
         .sort({ createdAt: -1 })
         .toArray();
+      res.json(result);
+    });
+
+    // class delete
+    app.delete("/classDelete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ClassCollection.deleteOne(query);
       res.json(result);
     });
 
