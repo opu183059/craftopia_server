d3eb5c568e4fcbe097a9d6fede9a11949dcd3008
opu183059 <opuctg440@gmail.com>
2023-06-07 12:32:03 +0600

    Server started

---
+++ index.js
@@ -0,0 +1,66 @@
+// craftopia
+// jCxDazOvTvBSQ2uH
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.USER_PASS);
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ljsyrma.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const UsersData = client.db('criftopia').collection('users')
+    const ClassCollection = client.db('criftopia').collection('class')
+    const ClassBooking = client.db('criftopia').collection('bookings')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+    res.send("server is running");
+  });
+  
+  app.listen(port, () => {
+    console.log(`this server is running on port ${port}`);
+  });
